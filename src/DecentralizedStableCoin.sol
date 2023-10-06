cd9b93bfe1e8ce65acb00c28e00082f3c75c1fd2
Sardius <0xsardius@gmail.com>
2023-10-06 14:24:08 -0400

    contract skeleton built

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier:MIT
+
+// Layout of contract
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title Decentralized Stable Coin
+ * @author Sardius
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by the DSCEngine. This contract is just the ERC20 implentation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin {
+
+}
