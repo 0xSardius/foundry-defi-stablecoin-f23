2f51c7c3416b1a597641beb5b1241001447c19fe
Sardius <0xsardius@gmail.com>
2023-10-06 14:57:34 -0400

    Fixed Ownable issue

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -22,16 +22,17 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {
-
+    constructor(address initialOwner) ERC20("DecentralizedStableCoin", "DSC") Ownable(initialOwner) {
+        initialOwner = msg.sender;
     }
+    
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if(_amount <= 0) {
             revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
-        if (_balance < _amount) {
+        if (balance < _amount) {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
